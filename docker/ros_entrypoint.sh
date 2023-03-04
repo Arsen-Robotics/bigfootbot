bec76f05918a929893a17973cad9ec507bdeaebc
jevgenik <jevgeni.kalbin@gmail.com>
2023-03-04 20:49:33 +0200

    husarnet

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -20,7 +20,8 @@ set -e
 #source ~/ros2_ws/install/setup.bash
 echo "source ${ROS_WS}/install/setup.bash" >> ~/.bashrc
 source ${ROS_WS}/install/setup.bash
-#source /ros_ws/install/local_setup.bash
+#. ~/.bashrc - alternative to calling "source ${ROS_WS}/install/setup.bash" above
+#source /${ROS_WS}/install/local_setup.bash - calling this will make available ONLY packages installed in ${ROS_WS} directory
 
 # This line is from default ros_entrypoint.sh script file
 #source "/opt/ros/$ROS_DISTRO/setup.bash"
--- docker/scripts/start_robot.sh
+++ docker/scripts/start_robot.sh
@@ -8,5 +8,5 @@ docker run -it \
 --rm \
 --env="DISPLAY" \
 --env="QT_X11_NO_MITSHM=1" \
---volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \  #  read-write mode (i.e. the container can write as well as read files on the host)
 aarch64_humble_realsense

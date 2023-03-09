819470a785db3122e929a8f33c5a8dc30c1f6580
Jevgeni Kalbin <jevgeni.kalbin@gmail.com>
2023-03-09 21:21:19 +0200

    update

--- docker/scripts/launch.sh
+++ docker/scripts/launch.sh
@@ -6,8 +6,8 @@ ros2 launch foxglove_bridge foxglove_bridge_launch.xml port:=8765 &
   
 # Launch Realsense node
 #./launch_realsense.sh &
-ros2 launch realsense2_camera rs_launch.py &
-#ros2 launch realsense2_camera rs_camera.launch depth_width:=640 depth_height:=480 depth_fps:=15
+#ros2 launch realsense2_camera rs_launch.py &
+ros2 launch realsense2_camera rs_launch.py rgb_camera.profile:=1280x720x30 &
   
 # Wait for any process to exit
 wait -n

0c20a6408ce51d00a12c4fae4a5838b3e1eb1897
Jevgeni Kalbin <jevgeni.kalbin@gmail.com>
2023-03-13 20:10:20 +0200

    roboclow config

--- docker/scripts/launch.sh
+++ docker/scripts/launch.sh
@@ -10,6 +10,8 @@ ros2 launch foxglove_bridge foxglove_bridge_launch.xml port:=8765 &
 ros2 launch realsense2_camera rs_launch.py rgb_camera.profile:=640x480x30 &
 
 ros2 launch bigfootbot_bringup ros2_roboclaw_driver.launch.py &
+
+rqt
   
 # Wait for any process to exit
 wait -n

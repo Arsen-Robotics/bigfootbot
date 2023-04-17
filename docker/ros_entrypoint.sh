3a9b12171edbcb9fcbee126569cd2f0e25da8f38
Jevgeni Kalbin <jevgeni.kalbin@gmail.com>
2023-04-17 21:13:04 +0300

    updated docker file ...bigfootbot_drive

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -37,8 +37,10 @@ source ${ROS_WS}/install/setup.bash
 #export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/ros_ws/src/bigfootbot_description/models/
 #export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/ros_ws/src/basic_mobile_robot/models/
 
-sudo apt-get update
-rosdep update
+# ----- NB! Does it needed?
+#sudo apt-get update
+#rosdep update
+# -----
 
 # If you have an image with an entrypoint pointing to entrypoint.sh, and you run 
 # your container as docker run my_image server start, that will translate 

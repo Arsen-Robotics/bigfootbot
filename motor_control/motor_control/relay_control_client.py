86b97e72450e32ce6d76f512d98587a6476da008
Jevgeni Kalbin <jevgeni.kalbin@gmail.com>
2024-02-20 17:05:05 +0200

    worked with relay control with keyboard

--- motor_control/motor_control/relay_control_client.py
+++ motor_control/motor_control/relay_control_client.py
@@ -1,39 +1,51 @@
+import sys
+from bfb_interfaces.srv import RelayControl
 import rclpy
 from rclpy.node import Node
-from bfb_interfaces.srv import RelayControl
-import time
+from pynput import keyboard
 
 class RelayControlClient(Node):
     def __init__(self):
         super().__init__('relay_control_client')
-        self.client = self.create_client(RelayControl,'relay_control')
+        self.client = self.create_client(RelayControl, 'relay_control')
 
         while not self.client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Service not available, waiting again...')
+            self.get_logger().info('service not available, waiting again...')
+        
+        self.request = RelayControl.Request()
 
-    def send_relay_control_request(self):
-        request = RelayControl.Request()
-        request.state = "on"
-        self.future = self.client.call_async(request)
+    def send_request(self, state):
+        self.request.state = state
+        self.future = self.client.call_async(self.request)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
+    
+    def on_press(self, key):
+        try:
+            if key.char == 'w':
+                response = self.send_request("on")
+                self.get_logger().info(f"{response.confirmation}")
+            elif key.char == 's':
+                response = self.send_request("off")
+                self.get_logger().info(f"{response.confirmation}")
+        except AttributeError:
+            pass
 
-def main(args=None):
-    rclpy.init(args=args)
+
+def main():
+    rclpy.init()
+
     relay_control_client = RelayControlClient()
-    relay_control_client.send_relay_control_request()
-    while rclpy.ok():
-        rclpy.spin_once(relay_control_client)
-        if relay_control_client.future.done():
-            try:
-                response = relay_control_client.future.result()
-            except Exception as e:
-                relay_control_client.get_logger().info(
-                    f"Service call failed {e}")
-            else:
-                relay_control_client.get_logger().info(response.confirmation)
-            break
+    #response = relay_control_client.send_request("on")
+    #relay_control_client.get_logger().info(f"{response.confirmation}")
+
+    listener = keyboard.Listener(on_press=relay_control_client.on_press)
+    listener.start()
+    listener.join()
 
     relay_control_client.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()

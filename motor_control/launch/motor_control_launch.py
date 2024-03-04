2838bff4c65d3775e873c49fcaebc77e3f76aa84
arsenikalbin <arseni.kalbin@gmail.com>
2024-03-04 17:53:06 +0200

    modified motor_control launch file, bfb_teleop dockerfile, commented out xml in ros_entrypoint.sh, fixed roboclaw udev rule in motor_control pkg

--- motor_control/launch/motor_control_launch.py
+++ motor_control/launch/motor_control_launch.py
@@ -1,24 +1,34 @@
-import os
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-# This is a Python function that returns the path to the package share directory
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+#from launch.launch_description_sources import PythonLaunchDescriptionSource
+#from launch_yaml.launch_description_sources import YAMLLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+import os
+from ament_index_python import get_package_share_directory
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='motor_control',
-            executable='joy_to_twist_node',
-            name='joy_to_twist_node'
-        ),
-        Node(
-            package='motor_control',
-            executable='roboclaw_control_node',
-            name='roboclaw_control_node'
-        ),
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('foxglove_bridge'), 'launch', 'foxglove_bridge_launch.xml')])
-    ])
+    ld = LaunchDescription()
+
+    foxglove_bridge_launch_file = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('foxglove_bridge'),
+                         'launch/foxglove_bridge_launch.xml')
+        )
+    )
+
+    joy_to_twist_node = Node(
+        package = 'motor_control',
+        executable = 'joy_to_twist_node'
+    )
+
+    roboclaw_control_node = Node(
+        package = 'motor_control',
+        executable = 'roboclaw_control_node'
+    )
+
+    ld.add_action(foxglove_bridge_launch_file)
+    ld.add_action(joy_to_twist_node)
+    ld.add_action(roboclaw_control_node)
+
+    return ld

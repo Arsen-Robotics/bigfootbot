0caf2bb10592099360420adc985e1974af254903
Jevgeni Kalbin <jevgeni.kalbin@gmail.com>
2024-05-22 14:10:47 +0300

    web/WebRTC

--- docker/web/WebRTC/app/public/scripts/index.js
+++ docker/web/WebRTC/app/public/scripts/index.js
@@ -13,3 +13,25 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: true })
   .catch(error => {
     console.warn(error.message);
 });
+
+
+// 
+this.io.on("connection", socket => { // .on listens for an event
+  const existingSocket = this.activeSockets.find(
+    existingSocket => existingSocket === socket.id
+  );
+
+  if (!existingSocket) {
+    this.activeSockets.push(socket.id);
+
+    socket.emit("update-user-list", { // .emit sends a message to the client that is connected
+      users: this.activeSockets.filter(
+        existingSocket => existingSocket !== socket.id
+      )
+    });
+
+    socket.broadcast.emit("update-user-list", { // .broadcast.emit sends a message to all clients except the one that is connected
+      users: [socket.id]
+    });
+  }
+}

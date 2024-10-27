53ff5805af4169594e0ac6d3dbf6905c6551fd02
arsenikalbin <arseni.kalbin@gmail.com>
2024-10-27 14:23:04 +0200

    simplified camera servo quickview logic

--- bfb_firmware/bfb_arduino/src/main.cpp
+++ bfb_firmware/bfb_arduino/src/main.cpp
@@ -16,11 +16,11 @@ int lastPanAngle = 0;
 
 // Angle for quick look to left/right
 int lookAngle = 60;
-bool lookToLeftSide = false;
-bool lookToRightSide = false;
+// bool lookToLeftSide = false;
+// bool lookToRightSide = false;
 
-unsigned long lastCommandTime = 0;  // Last time a command was received
-unsigned long returnDelay = 100;   // Time (in ms) to wait after qucick look button was released, before returning to neutral position
+// unsigned long lastCommandTime = 0;  // Last time a command was received
+// unsigned long returnDelay = 100;   // Time (in ms) to wait after qucick look button was released, before returning to neutral position
 
 void setup() {
   Serial.begin(9600);
@@ -49,7 +49,8 @@ void loop() {
     if (command == "0") {
       tiltServo.write(tiltNeutral); // Set servo 1 to neutral position
       lastTiltAngle = tiltNeutral; // Update last angle
-
+    }
+    if (command == "11") {
       panServo.write(panNeutral); // Set servo 2 to neutral position
       lastPanAngle = panNeutral; // Update last angle
     }
@@ -62,22 +63,24 @@ void loop() {
       tiltServo.write(lastTiltAngle); // Decrement servo 1 angle by -2 deg
     }
     if (command == "3") {
-      lastCommandTime = millis();  // Update last command time
+      // lastCommandTime = millis();  // Update last command time
 
-      if (lookToLeftSide == false || lookToRightSide == true) {
-        lookToLeftSide = true;
-        lookToRightSide = false;
-        panServo.write(panNeutral + lookAngle); // Look to the left
-      }
+      // if (lookToLeftSide == false || lookToRightSide == true) {
+      // lookToLeftSide = true;
+      // lookToRightSide = false;
+      panServo.write(panNeutral + lookAngle); // Look to the left
+      lastPanAngle = panNeutral + lookAngle;
+      // }
     }
     if (command == "4") {
-      lastCommandTime = millis();  // Update last command time
+      // lastCommandTime = millis();  // Update last command time
 
-      if (lookToRightSide == false || lookToLeftSide == true) {
-        lookToRightSide = true;
-        lookToLeftSide = false;
-        panServo.write(panNeutral - lookAngle); // Look to the right
-      }
+      // if (lookToRightSide == false || lookToLeftSide == true) {
+      // lookToRightSide = true;
+      // lookToLeftSide = false;
+      panServo.write(panNeutral - lookAngle); // Look to the right
+      lastPanAngle = panNeutral - lookAngle;
+      // }
     }
     if (command == "5") {
       lastPanAngle = constrain(lastPanAngle + 3, 0, 180); // Ensure angle stays between 0 and 180
@@ -101,13 +104,13 @@ void loop() {
     }
   }
   // If no quick look command was received for a while, return to neutral position
-  if ((millis() - lastCommandTime > returnDelay) && (lookToLeftSide || lookToRightSide)) {
-    panServo.write(panNeutral);  // Move pan back to neutral
-    lastPanAngle = panNeutral;
-    lookToLeftSide = false;
-    lookToRightSide = false;
+  // if ((millis() - lastCommandTime > returnDelay) && (lookToLeftSide || lookToRightSide)) {
+  //   panServo.write(panNeutral);  // Move pan back to neutral
+  //   lastPanAngle = panNeutral;
+  //   lookToLeftSide = false;
+  //   lookToRightSide = false;
 
-    //delay(200);  // Wait for the servo to reach the neutral position
-    // panServo.detach();  // Detach the servo to stop it from auto adjusting
-  }
+  //   //delay(200);  // Wait for the servo to reach the neutral position
+  //   // panServo.detach();  // Detach the servo to stop it from auto adjusting
+  // }
 }

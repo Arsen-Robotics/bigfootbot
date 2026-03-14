a8c4ac09da29fcf3f6d4e676feca0078b8f47a26
Arseni Kalbin <61424435+arsenikalbin@users.noreply.github.com>
2026-03-14 18:32:46 +0200

    Update Docker installation script for Ubuntu and Debian

--- docs_and_scripts/bfb_computer_setup.sh
+++ docs_and_scripts/bfb_computer_setup.sh
@@ -51,20 +51,24 @@ while true; do
             echo "Installing Docker on Ubuntu..."
 
             # Uninstall conflicting packages
-            for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+            for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
 
             # Add Docker's official GPG key:
             sudo apt-get update
-            sudo apt-get install ca-certificates curl
+            sudo apt-get install -y ca-certificates curl
             sudo install -m 0755 -d /etc/apt/keyrings
             sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
             sudo chmod a+r /etc/apt/keyrings/docker.asc
-
+            
             # Add the repository to Apt sources:
-            echo \
-            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            sudo tee /etc/apt/sources.list.d/docker.sources <<-EOF
+            Types: deb
+            URIs: https://download.docker.com/linux/ubuntu
+            Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+            Components: stable
+            Signed-By: /etc/apt/keyrings/docker.asc
+            EOF
+            
             sudo apt-get update
 
             # Install Docker
@@ -78,20 +82,24 @@ while true; do
             echo "Installing Docker on Debian..."
 
             # Uninstall conflicting packages
-            for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+            for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove -y $pkg; done
 
             # Add Docker's official GPG key:
             sudo apt-get update
-            sudo apt-get install ca-certificates curl
+            sudo apt-get install -y ca-certificates curl
             sudo install -m 0755 -d /etc/apt/keyrings
-            sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+            sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
             sudo chmod a+r /etc/apt/keyrings/docker.asc
-
+            
             # Add the repository to Apt sources:
-            echo \
-            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
-            $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-            sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+            sudo tee /etc/apt/sources.list.d/docker.sources <<-EOF
+            Types: deb
+            URIs: https://download.docker.com/linux/ubuntu
+            Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+            Components: stable
+            Signed-By: /etc/apt/keyrings/docker.asc
+            EOF
+            
             sudo apt-get update
 
             # Install Docker

c64f040cb33a8e4ca5b9ba3f492b9f44117521e1
Arseni Kalbin <61424435+arsenikalbin@users.noreply.github.com>
2026-03-14 18:36:58 +0200

    Fix heredoc syntax for Docker repository setup

--- docs_and_scripts/bfb_computer_setup.sh
+++ docs_and_scripts/bfb_computer_setup.sh
@@ -61,13 +61,13 @@ while true; do
             sudo chmod a+r /etc/apt/keyrings/docker.asc
             
             # Add the repository to Apt sources:
-            sudo tee /etc/apt/sources.list.d/docker.sources <<-EOF
-            Types: deb
-            URIs: https://download.docker.com/linux/ubuntu
-            Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-            Components: stable
-            Signed-By: /etc/apt/keyrings/docker.asc
-            EOF
+            sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
             
             sudo apt-get update
 
@@ -92,13 +92,13 @@ while true; do
             sudo chmod a+r /etc/apt/keyrings/docker.asc
             
             # Add the repository to Apt sources:
-            sudo tee /etc/apt/sources.list.d/docker.sources <<-EOF
-            Types: deb
-            URIs: https://download.docker.com/linux/ubuntu
-            Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-            Components: stable
-            Signed-By: /etc/apt/keyrings/docker.asc
-            EOF
+            sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
             
             sudo apt-get update
 
